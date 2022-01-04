ab58b16c2e0b3207749a4fabadfb178a029da04e
Tom Longhurst <tom.longhurst@asos.com>
2022-01-04 10:33:33 +0000

    Documentation

--- TomLonghurst.EnumerableAsyncProcessor/Interfaces/IAsyncProcessor.cs
+++ TomLonghurst.EnumerableAsyncProcessor/Interfaces/IAsyncProcessor.cs
@@ -2,7 +2,24 @@
 
 public interface IAsyncProcessor<TResult>
 {
+    /**
+     * <summary>
+     * A collection of all the asynchronous Tasks, which could be pending or complete.
+     * </summary>
+     */
     IEnumerable<Task<TResult>> GetEnumerableTasks();
+    
+    /**
+     * <summary>
+     * A task that will contain the mapped results when complete
+     * </summary>
+     */
     Task<IEnumerable<TResult>> GetResults();
+    
+    /**
+     * <summary>
+     * A task representing the overall progress.
+     * </summary>
+     */
     Task ContinuationTask { get; }
 }

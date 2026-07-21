691ecf54a0d3b99b0bba14752c0336d46428ea2a
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2026-07-21 20:25:05 +0100

    perf: use worker pool in InParallelAsync (#355)

--- EnumerableAsyncProcessor/Extensions/ParallelExtensions.cs
+++ EnumerableAsyncProcessor/Extensions/ParallelExtensions.cs
@@ -1,123 +1,207 @@
-﻿namespace EnumerableAsyncProcessor.Extensions;
+using System.Collections.Concurrent;
+
+namespace EnumerableAsyncProcessor.Extensions;
 
 public static class ParallelExtensions
 {
-    public static async Task InParallelAsync<TSource, TResult>(
+    public static Task InParallelAsync<TSource, TResult>(
         this IEnumerable<TSource> source,
         int levelOfParallelism,
         Func<TSource, Task<TResult>> taskSelector)
     {
-        await InParallelAsync(source, levelOfParallelism, taskSelector, CancellationToken.None).ConfigureAwait(false);
+        return InParallelAsync(source, levelOfParallelism, taskSelector, CancellationToken.None);
     }
 
-    public static async Task InParallelAsync<TSource, TResult>(
+    public static Task InParallelAsync<TSource, TResult>(
         this IEnumerable<TSource> source,
         int levelOfParallelism,
         Func<TSource, Task<TResult>> taskSelector,
         CancellationToken cancellationToken)
     {
-        if (levelOfParallelism <= 0)
-        {
-            levelOfParallelism = Environment.ProcessorCount;
-        }
-        
-        using var parallelLock = new SemaphoreSlim(initialCount:levelOfParallelism, maxCount:levelOfParallelism);
-
-        await Task.WhenAll(source.Select(item => ProcessAsync(item, taskSelector, parallelLock, cancellationToken))).ConfigureAwait(false);
+        return StartWorkers(source, levelOfParallelism, taskSelector, cancellationToken);
     }
-    
-    public static async Task InParallelAsync<TSource>(
+
+    public static Task InParallelAsync<TSource>(
         this IEnumerable<TSource> source,
         int levelOfParallelism,
         Func<TSource, Task> taskSelector)
     {
-        await InParallelAsync(source, levelOfParallelism, taskSelector, CancellationToken.None).ConfigureAwait(false);
+        return InParallelAsync(source, levelOfParallelism, taskSelector, CancellationToken.None);
     }
 
-    public static async Task InParallelAsync<TSource>(
+    public static Task InParallelAsync<TSource>(
         this IEnumerable<TSource> source,
         int levelOfParallelism,
         Func<TSource, Task> taskSelector,
         CancellationToken cancellationToken)
     {
-        if (levelOfParallelism <= 0)
-        {
-            levelOfParallelism = Environment.ProcessorCount;
-        }
-        
-        using var parallelLock = new SemaphoreSlim(initialCount:levelOfParallelism, maxCount:levelOfParallelism);
-
-        await Task.WhenAll(source.Select(item => ProcessAsync(item, taskSelector, parallelLock, cancellationToken))).ConfigureAwait(false);
+        return StartWorkers(source, levelOfParallelism, taskSelector, cancellationToken);
     }
 
     // Overloads for CPU-bound processing
-    public static async Task InParallelAsync<TSource, TResult>(
+    public static Task InParallelAsync<TSource, TResult>(
         this IEnumerable<TSource> source,
         int levelOfParallelism,
         Func<TSource, TResult> taskSelector,
         CancellationToken cancellationToken = default)
     {
-        await InParallelAsync(source, levelOfParallelism, item => Task.FromResult(taskSelector(item)), cancellationToken).ConfigureAwait(false);
+        return StartWorkers(
+            source,
+            levelOfParallelism,
+            item =>
+            {
+                _ = taskSelector(item);
+                return Task.CompletedTask;
+            },
+            cancellationToken);
     }
 
-    public static async Task InParallelAsync<TSource>(
+    public static Task InParallelAsync<TSource>(
         this IEnumerable<TSource> source,
         int levelOfParallelism,
         Action<TSource> taskSelector,
         CancellationToken cancellationToken = default)
     {
-        await InParallelAsync(source, levelOfParallelism, item => { taskSelector(item); return Task.CompletedTask; }, cancellationToken).ConfigureAwait(false);
-    }
-
-    private static async Task<TResult> ProcessAsync<TSource, TResult>(
-        TSource item,
-        Func<TSource, Task<TResult>> taskSelector,
-        SemaphoreSlim parallelLock,
-        CancellationToken cancellationToken)
-    {
-        var semaphoreAcquired = false;
-        
-        try
-        {
-            await parallelLock.WaitAsync(cancellationToken).ConfigureAwait(false);
-            semaphoreAcquired = true;
-            
-            cancellationToken.ThrowIfCancellationRequested();
-
-            return await taskSelector(item).ConfigureAwait(false);
-        }
-        finally
-        {
-            if (semaphoreAcquired)
+        return StartWorkers(
+            source,
+            levelOfParallelism,
+            item =>
             {
-                parallelLock.Release();
-            }
-        }
+                taskSelector(item);
+                return Task.CompletedTask;
+            },
+            cancellationToken);
     }
-    
-    private static async Task ProcessAsync<TSource>(
-        TSource item,
+
+    private static Task StartWorkers<TSource>(
+        IEnumerable<TSource> source,
+        int levelOfParallelism,
         Func<TSource, Task> taskSelector,
-        SemaphoreSlim parallelLock,
         CancellationToken cancellationToken)
     {
-        var semaphoreAcquired = false;
-        
+        TSource[] items;
+
         try
         {
-            await parallelLock.WaitAsync(cancellationToken).ConfigureAwait(false);
-            semaphoreAcquired = true;
-            
-            cancellationToken.ThrowIfCancellationRequested();
-
-            await taskSelector(item).ConfigureAwait(false);
+            items = source.ToArray();
         }
-        finally
+        catch (Exception exception)
         {
-            if (semaphoreAcquired)
+            return Task.FromException(exception);
+        }
+
+        if (items.Length == 0)
+        {
+            return Task.CompletedTask;
+        }
+
+        if (levelOfParallelism <= 0)
+        {
+            levelOfParallelism = Environment.ProcessorCount;
+        }
+
+        var workerCount = Math.Min(levelOfParallelism, items.Length);
+        var completionSource = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+        var exceptions = new ConcurrentQueue<Exception>();
+        var nextIndex = -1;
+        var remainingWorkers = workerCount;
+        var wasCanceled = 0;
+
+        for (var i = 0; i < workerCount; i++)
+        {
+            _ = Task.Run(async () =>
             {
-                parallelLock.Release();
+                try
+                {
+                    while (true)
+                    {
+                        if (cancellationToken.IsCancellationRequested)
+                        {
+                            Interlocked.Exchange(ref wasCanceled, 1);
+                            return;
+                        }
+
+                        var index = Interlocked.Increment(ref nextIndex);
+
+                        if (index >= items.Length)
+                        {
+                            return;
+                        }
+
+                        Task? task = null;
+
+                        try
+                        {
+                            cancellationToken.ThrowIfCancellationRequested();
+                            task = taskSelector(items[index]);
+                            await task.ConfigureAwait(false);
+                        }
+                        catch (OperationCanceledException)
+                        {
+                            Interlocked.Exchange(ref wasCanceled, 1);
+                        }
+                        catch (Exception exception)
+                        {
+                            EnqueueExceptions(exceptions, task, exception);
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    exceptions.Enqueue(exception);
+                }
+                finally
+                {
+                    if (Interlocked.Decrement(ref remainingWorkers) == 0)
+                    {
+                        Complete(completionSource, exceptions, wasCanceled, cancellationToken);
+                    }
+                }
+            });
+        }
+
+        return completionSource.Task;
+    }
+
+    private static void EnqueueExceptions(
+        ConcurrentQueue<Exception> exceptions,
+        Task? task,
+        Exception exception)
+    {
+        if (task is { IsFaulted: true })
+        {
+            foreach (var innerException in task.Exception!.InnerExceptions)
+            {
+                exceptions.Enqueue(innerException);
             }
+
+            return;
+        }
+
+        exceptions.Enqueue(exception);
+    }
+
+    private static void Complete(
+        TaskCompletionSource completionSource,
+        ConcurrentQueue<Exception> exceptions,
+        int wasCanceled,
+        CancellationToken cancellationToken)
+    {
+        if (!exceptions.IsEmpty)
+        {
+            completionSource.TrySetException(exceptions);
+        }
+        else if (wasCanceled != 0)
+        {
+            var canceledToken = cancellationToken.IsCancellationRequested
+                ? cancellationToken
+                : new CancellationToken(canceled: true);
+
+            completionSource.TrySetCanceled(canceledToken);
+        }
+        else
+        {
+            completionSource.TrySetResult();
         }
     }
 }
